de03a3cec05cdaba1266ec1a98c046eef21c8c3f
Malve <ilyabagrov06@mail.ru>
2026-07-01 15:16:55 +0300

    наверное конечная версия эмулятора
    добавил файл, который обновляет .json файл, чтобы в нём были нормальные значения всегда, так же добавил период восстановления после аварии, попадание в аварию после окончания топлива и вроде что то ещё было

--- emulator/main.py
+++ emulator/main.py
@@ -3,18 +3,21 @@ import json
 from math import sqrt
 import time
 
+#frame - секунд между кадрами
 frame = 10
+#countOfFrame - номер конкретного кадра
+countOfFrame = 0
 
+#Расчитывает вероятность попадения в аварию
 def accident_probability(data):
-        if random.randrange(0,100) > data['chance']:
-            data['chance'] += random.randrange(2,8)
+        if random.randint(1, 100) > data['chance']:
+            data['chance'] = min(100, data['chance'] + random.randint(2, 5))
             return 0
         else:
-            data['chance'] -= 30
-            if data['chance'] < 0:
-                data['chance'] = 0
+            data['chance'] = max(0, data['chance'] - 50)
             return 1
 
+#Изменяет значение элемента в зависимости от minVal, maxVal, результатов расчёта аварии и интервала изменения данных
 def change_value(value, minVal, maxVal, changeInterval, resChanse):
     if value is None:
         value = (minVal + maxVal) // 2
@@ -23,17 +26,19 @@ def change_value(value, minVal, maxVal, changeInterval, resChanse):
     maxVal = int(maxVal)
 
     if resChanse == 0:
+        #расчитывает, на сколько изменятся данные
         change = random.randrange(-changeInterval, changeInterval)
         new_value = value + change
-        # Ограничиваем в нормальном режиме
         return max(minVal, min(maxVal, new_value))
-    else:  # авария – выход за границы разрешён
+    else:
+        #В случае попадения в аварию считает значение, на которое параметр будет больше или меньше (в зависимости к чему ближе) максимума или минимума
         change = random.randrange(0, changeInterval * 3)
         if value - minVal >= maxVal - value:
             return maxVal + change
         else:
             return minVal - change
 
+#Расчёт значений для характеристик, которые только уменьшаются, как бензин или заряд у дронов
 def minuend_value(value, changeInterval):
     changeOfValue = random.randrange(0, changeInterval)
     if value  - changeOfValue >= 0:
@@ -41,75 +46,115 @@ def minuend_value(value, changeInterval):
     else:
         return 0
 
-def change_coordinates(coord, startcoord, endcoord, speed):
+#Изменение координат в зависимости от скорости и расстояния
+def change_coordinates(coord, endcoord, speed):
     global frame
     if speed is None:
         return coord
     step = speed / 3600 * frame
     dx = endcoord[0] - coord[0]
     dy = endcoord[1] - coord[1]
+    #Расстояние между объектом и конечной точкой
     dist = sqrt(dx**2 + dy**2)
+    #Если очень близко
     if dist <= step:
         coord[0], coord[1] = endcoord[0], endcoord[1]
     else:
+        #Расчёт получившихся координат по пропорции
         coord[0] += step * dx / dist
         coord[1] += step * dy / dist
     return coord
 
+#Функция, которая изменяет все данные
 def change_data(data):
     resultOfChanse = accident_probability(data)
-    if data['type'] == "car":
-        data['Fuel'] = minuend_value(data['Fuel'], 4)
-        data['Speed'] = change_value(data['Speed'], 40, 130, 10, resultOfChanse)
-        data['coordinates'] = change_coordinates(data['coordinates'], data['start'], data['end'], data['Speed'])
-    elif data['type'] == 'azs':
-        data['fuel_level'] = minuend_value(data['fuel_level'], 2)
-        data['occupancy'] = change_value(data['occupancy'], 0, 85, 5, resultOfChanse)
-    elif data['type'] == 'sklad':
-        data['goods_load'] = change_value(data['goods_load'], 0, 85, 5, resultOfChanse)
+    global countOfFrame
+    #Смотрит, была ли авария, и если была, то данные не меняются
+    if not data['accident']:
+        #Изменение для машин
+        if data['type'] == "car":
+            data['Fuel'] = minuend_value(data['Fuel'], 4)
+            data['temperature'] = change_value(data['temperature'], 90, 110, 30, resultOfChanse)
+            data['Speed'] = change_value(data['Speed'], 60, 110, 20, resultOfChanse)
+            data['coordinates'] = change_coordinates(data['coordinates'], data['end'], data['Speed'])
+            #Если топливо на нуле, то тоже авария
+            if data['Fuel'] <= 0:
+                data['accident'] = True
+                data['timeOfAc'] = countOfFrame
+        #Тоже самое, только для дронов
+        elif data['type'] == "drn":
+            data['charge'] = minuend_value(data['charge'], 4)
+            data['propeller_rpm'] = change_value(data['propeller_rpm'], 3000, 8000, 1000, resultOfChanse)
+            data['speed'] = change_value(data['speed'], 60, 110, 20, resultOfChanse)
+            data['altitude'] = change_value(data['altitude'], 10, 300, 3, resultOfChanse)
+            data['coordinates'] = change_coordinates(data['coordinates'], data['end'], data['speed'])   
 
-        temper = data.get('temperature')
-        if temper is None:
-            temper = 0
-        else:
-            temper = int(temper)
-        data['temperature'] = change_value(temper, 0, 30, 10, resultOfChanse)
-
-        humidity = data.get('humidity')
-        if humidity is None:
-            humidity = 50
-        else:
-            humidity = int(humidity)
-        data['humidity'] = change_value(humidity, 30, 80, 10, resultOfChanse)
-        data['trucks_loading'] = change_value(data['trucks_loading'], 3, 50, 1, resultOfChanse)
+            if data['charge'] <= 0:
+                data['accident'] = True
+                data['timeOfAc'] = countOfFrame   
+    #Если произошла авария, то данные, как скорость становятся нулями  
     else:
-        data['charge'] = minuend_value(data['charge'], 4)
-        data['propeller_rpm'] = change_value(data['propeller_rpm'], 3500, 8000, 100, resultOfChanse)
-        data['speed'] = change_value(data['speed'], 30, 130, 10, resultOfChanse)
-        data['altitude'] = change_value(data['altitude'], 10, 300, 3, resultOfChanse)
-        data['coordinates'] = change_coordinates(data['coordinates'], data['start'], data['end'], data['speed'])
+        if data['type'] == "car":
+            data['Fuel'] = 0
+            data['temperature'] = 0
+            data['Speed'] = 0
 
+        elif data['type'] == "drn":
+            data['charge'] = 0
+            data['propeller_rpm'] = 0
+            data['speed'] = 0
+            data['altitude'] = 0
+    #Простое изменение состояний для азс и складов
+    if data['type'] == 'gas':
+        data['fuel_level'] = minuend_value(data['fuel_level'], 2)
+        data['price'] = change_value(data['price'], 0, 85, 5, resultOfChanse)
+        data['occupancy'] = change_value(data['occupancy'], 0, 85, 5, resultOfChanse)
+    elif data['type'] == 'wrh':
+        data['goods_load'] = change_value(data['goods_load'], 0, 85, 5, resultOfChanse)
+        data['temperature'] = change_value(data['temperature'], 10, 25, 5, resultOfChanse)
+        data['humidity'] = change_value(data['humidity'], 30, 70, 10, resultOfChanse)
+        data['trucks_loading'] = change_value(data['trucks_loading'], 3, 50, 1, resultOfChanse)
+    if resultOfChanse and not data['accident']:
+        data['accident'] = True
+        data['timeOfAc'] = countOfFrame
+
+#смотрит для каждого автомобиля, есть ли рядом заправка, и если есть, то машина заправляется 
 def azs_and_truks(data):
     for rows1 in data:
         if rows1['type'] == "car":
             for rows2 in data:
-                if rows2['type'] == 'azs':
+                if rows2['type'] == 'gas':
+                    #Тут сравниваются координаты по обычной гиперболе
                     if sqrt((rows1['coordinates'][0]-rows2['coordinates'][0])**2 + (rows1['coordinates'][1]-rows2['coordinates'][1])**2) <= 1:
                         rows1['Fuel'] = 100
 
+#Простой вывод данных в консоль с разделителем
 def print_data(data):
     for i in data:
             if i['type'] == "car":
-                print(str(i['id']) + '#' + str(i['type']) + '#' + str(i['Fuel']) + '#' + str(i['Speed']) + '#' + str(i['coordinates'][0]) + '#' + str(i['coordinates'][1]) + '#' + str(i['start'][0]) + '#' + str(i['start'][1]) + '#' + str(i['end'][0]) + '#' + str(i['end'][1]))
-            elif i['type'] == 'azs':
-                print(str(i['id']) + '#' + str(i['type']) + '#' + str(i['coordinates'][0]) + '#' + str(i['coordinates'][1]) + '#' + str(i['fuel_level']) + '#' + str(i['fuel_type']) + '#' + str(i['price']) + '#' + str(i['occupancy']))
-            elif i['type'] == 'sklad':
-                print(str(i['id']) + '#' + str(i['type']) + '#' + str(i['coordinates'][0]) + '#' + str(i['coordinates'][1]) + '#' + str(i['goods_load']) + '#' + str(i['temperature']) + '#' + str(i['humidity']) + '#' + str(i['trucks_loading']))
+                print(str(i['type']) + "_" + str(i['id']) + '#' + str(i['Fuel']) + '#' + str(i['Speed']) + '#' + str(i['coordinates'][0]) + '#' + str(i['coordinates'][1]) + '#' + str(i['start'][0]) + '#' + str(i['start'][1]) + '#' + str(i['end'][0]) + '#' + str(i['end'][1]))
+            elif i['type'] == 'gas':
+                print(str(i['type']) + "_" + str(i['id']) + '#' + str(i['coordinates'][0]) + '#' + str(i['coordinates'][1]) + '#' + str(i['fuel_level']) + '#' + str(i['fuel_type']) + '#' + str(i['price']) + '#' + str(i['occupancy']))
+            elif i['type'] == 'wrh':
+                print(str(i['type']) + "_" + str(i['id']) + '#' + str(i['coordinates'][0]) + '#' + str(i['coordinates'][1]) + '#' + str(i['goods_load']) + '#' + str(i['temperature']) + '#' + str(i['humidity']) + '#' + str(i['trucks_loading']))
             else:
-                print(str(i['id']) + '#' + str(i['type']) + '#' + str(i['coordinates'][0]) + '#' + str(i['coordinates'][1]) + '#' + str(i['charge']) + '#' + str(i['propeller_rpm']) + '#' + str(i['speed']) + '#' + str(i['altitude']) + '#' + str(i['start'][0]) + '#' + str(i['start'][1]) + '#' + str(i['end'][0]) + '#' + str(i['end'][1]))
+                print(str(i['type']) + "_" + str(i['id']) + '#' + str(i['coordinates'][0]) + '#' + str(i['coordinates'][1]) + '#' + str(i['charge']) + '#' + str(i['propeller_rpm']) + '#' + str(i['speed']) + '#' + str(i['altitude']) + '#' + str(i['start'][0]) + '#' + str(i['start'][1]) + '#' + str(i['end'][0]) + '#' + str(i['end'][1]))
 
+#Позволяет выходить из состояния аварии с прошестивем времени, которое у каждого объекта своё, при выходе топливо и заряд полные
+def recovery(data):
+    global countOfFrame
+    for i in data:
+        if i['accident']:
+            if i['timeOfAc'] + i['recovery'] <= countOfFrame:
+                i['accident'] = False
+                if i['type'] == "car":
+                    i['Fuel'] = 100
+                elif i['type'] == "drn":
+                    i['charge'] = 100
+
+#Главная функция
 def main_function():
-    with open('data.json', 'r') as f:
+    with open('data.json', 'r', encoding='utf-8-sig') as f:
         data = json.load(f)
 
     for row in data:
@@ -119,11 +164,15 @@ def main_function():
 
     print_data(data)
 
+    recovery(data)
+
     with open('data.json', 'w') as f:
         json.dump(data, f)
 
+#Бесконечный цикл с работой раз в frame секунд
 if __name__ == "__main__":
     while True:
+        countOfFrame+=1
         main_function()
         print("-----")
         time.sleep(frame)
---
+++ emulator/reset.py
@@ -0,0 +1,20 @@
+import json
+import random
+
+# Читаем с учётом возможного BOM
+with open('data.json', 'r', encoding='utf-8-sig') as f:
+    data = json.load(f)
+
+for obj in data:
+    obj['accident'] = False
+    obj['timeOfAc'] = 0
+    if 'Fuel' in obj:
+        obj['Fuel'] = random.randint(0, 100)
+    if 'charge' in obj:
+        obj['charge'] = random.randint(0, 100)
+
+# Записываем без BOM (стандартный UTF-8)
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, indent=2, ensure_ascii=False)
+
+print("✅ Данные успешно сброшены!")
