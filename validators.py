eb940d83b9baf463073fed502eceb9c07ff23720
RedsGits <9898063402@mail.ru>
2026-07-01 16:03:00 +0300

    Validators-1.1
    Обновил валидаторы в соответствии с новыми параметрами в БД

--- validators.py
+++ validators.py
@@ -3,10 +3,12 @@ from typing import Literal
 
 # 1. Машина
 class CarSchema(BaseModel):
-    id: str = Field(..., max_length=10)
+    id: int = Field(..., ge=1)
+    name: str = Field(..., max_length=100)
+    description: str = Field(..., max_length=500)
     fuel_level: int = Field(..., ge=0, le=100)
-    engine_temp: int = Field(..., ge=0, le=150)
-    speed: int = Field(..., ge=0, le=60)
+    engine_temp: int = Field(..., ge=0, le=200)
+    speed: int = Field(..., ge=0, le=170)
     latitude: float = Field(..., ge=-90, le=90)
     longitude: float = Field(..., ge=-180, le=180)
     start_lat: float = Field(..., ge=-90, le=90)
@@ -15,15 +17,17 @@ class CarSchema(BaseModel):
     end_lon: float = Field(..., ge=-180, le=180)
 
     def calculate_status(self) -> str:
-        if self.fuel_level < 5 or self.engine_temp >= 120:
+        if (self.fuel_level < 5 or self.engine_temp <= 40 or self.engine_temp >= 160 or self.speed <= 10 or self.speed >= 160):
             return "danger"
-        if self.fuel_level < 10 or self.engine_temp >= 110:
+        if (self.fuel_level < 10 or self.engine_temp < 90 or self.engine_temp > 110 or self.speed < 60 or self.speed > 110):
             return "warning"
         return "normal"
 
 # 2. АЗС
 class GasStationSchema(BaseModel):
-    id: str = Field(..., max_length=10)
+    id: int = Field(..., ge=1)
+    name: str = Field(..., max_length=100)
+    description: str = Field(..., max_length=500)
     latitude: float = Field(..., ge=-90, le=90)
     longitude: float = Field(..., ge=-180, le=180)
     fuel_level: int = Field(..., ge=0, le=100)
@@ -40,7 +44,9 @@ class GasStationSchema(BaseModel):
 
 # 3. Склад
 class WarehouseSchema(BaseModel):
-    id: str = Field(..., max_length=10)
+    id: int = Field(..., ge=1)
+    name: str = Field(..., max_length=100)
+    description: str = Field(..., max_length=500)
     capacity_used: int = Field(..., ge=0, le=100) 
     temperature: int = Field(..., ge=0, le=40)
     humidity: int = Field(..., ge=0, le=100)
@@ -49,15 +55,17 @@ class WarehouseSchema(BaseModel):
     longitude: float = Field(..., ge=-180, le=180)
 
     def calculate_status(self) -> str:
-        if self.capacity_used >= 95 or self.temperature <= 5 or self.temperature >= 30 or self.humidity <= 20 or self.humidity >= 80:
+        if (self.capacity_used >= 95 or self.temperature <= 5 or self.temperature >= 30 or self.humidity <= 20 or self.humidity >= 80):
             return "danger"
-        if self.capacity_used >= 80 or self.temperature <= 10 or self.temperature >= 25 or self.humidity <= 30 or self.humidity >= 60:
+        if (self.capacity_used >= 80 or self.temperature <= 10 or self.temperature >= 25 or self.humidity <= 30 or self.humidity >= 60):
             return "warning"
         return "normal"
 
 # 4. Дрон
 class DroneSchema(BaseModel):
-    id: str = Field(..., max_length=10)
+    id: int = Field(..., ge=1)
+    name: str = Field(..., max_length=100)
+    description: str = Field(..., max_length=500)
     battery: int = Field(..., ge=0, le=100)
     propeller_rpm: int = Field(..., ge=0, le=20000)
     speed: int = Field(..., ge=0, le=60)
@@ -70,8 +78,8 @@ class DroneSchema(BaseModel):
     end_lon: float = Field(..., ge=-180, le=180)
 
     def calculate_status(self) -> str:
-        if self.battery < 15 or self.propeller_rpm <= 1500 or self.propeller_rpm >= 10000:
+        if (self.battery < 15 or self.propeller_rpm <= 1500 or self.propeller_rpm >= 10000):
             return "danger"
-        if self.battery < 30 or self.propeller_rpm <= 3000 or self.propeller_rpm >= 8000:
+        if (self.battery < 30 or self.propeller_rpm <= 3000 or self.propeller_rpm >= 8000):
             return "warning"
         return "normal"
